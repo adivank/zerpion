69ad216efd8927fee78bd10e2813cb5ff170bd54
Adam Ivankovic <adam.ivankovic@gmail.com>
2024-08-26 19:36:41 +0200

    Changed app name

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -10,7 +10,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     host: process.env.DB_HOST || 'localhost',
     port: parseInt(process.env.DB_PORT) || 3306,
     username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASS || 'root',
+    password: process.env.DB_PASSWORD || 'root',
     database: process.env.DB_NAME || 'test',
     autoLoadEntities: true,
     synchronize: process.env.NODE_ENV === 'production' ? false : true,
