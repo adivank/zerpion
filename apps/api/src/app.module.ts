a3514c11cd0a1ed329e860ee56bbbcff60013da6
Adam Ivankovic <adam.ivankovic@gmail.com>
2024-09-16 13:59:40 +0200

    added turbo

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -9,6 +9,7 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
 // import { APP_GUARD } from '@nestjs/core';
 // import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
@@ -20,13 +21,14 @@ import { join } from 'path';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT) || 3306,
+      port: parseInt(process.env.DB_PORT) || 3307,
       username: process.env.DB_USERNAME || 'root',
       password: process.env.DB_PASSWORD || 'root',
-      database: process.env.DB_NAME || 'test',
+      database: process.env.DB_NAME || 'template_db',
       autoLoadEntities: true,
       synchronize: process.env.NODE_ENV === 'production' ? false : true,
     }),
+    ConfigModule.forRoot(),
     ProductModule,
     CartModule,
     CategoryModule,
